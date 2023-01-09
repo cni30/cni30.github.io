6e1f92ba97bae38cf58f00b83460ad2a46480bc5
cni30 <1362769051@qq.com>
2023-01-10 00:45:54 +0800

    Site updated: 2023-01-10 00:45:48

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,8 @@
 WIDGET = {
   "CONFIG": {
-    "modules": "10243",
-    "background": "1",
-    "tmpColor": "FFFFFF",
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FF9900",
     "tmpSize": "16",
     "cityColor": "FFFFFF",
     "citySize": "16",
@@ -11,12 +11,12 @@ WIDGET = {
     "weatherIconSize": "24",
     "alertIconSize": "18",
     "padding": "10px 10px 10px 10px",
-    "shadow": "1",
+    "shadow": "0",
     "language": "auto",
-    "borderRadius": "10",
+    "borderRadius": "5",
     "fixed": "false",
-    "vertical": "top",
-    "horizontal": "left",
+    "vertical": "center",
+    "horizontal": "center",
     "key": "7768566b572345c4afd8282556bb8c11"
   }
 }
